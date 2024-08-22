df652b4f8dccaacd74a70f5d338894af0f7d3d98
Arkadiy <ri.arkadiy@gmail.com>
2024-08-22 16:01:45 +0300

    init commit

---
+++ src/main/java/org/tix/lab3_1/Lab3ExpertModuleApplication.java
@@ -0,0 +1,13 @@
+package org.tix.lab3_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3ExpertModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3ExpertModuleApplication.class, args);
+    }
+
+}
